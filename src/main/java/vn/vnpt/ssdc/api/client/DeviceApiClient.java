3877cc3fcc4c1e9a9948899d17f3a236fb61a043
Hieu Pham <hieu.fet@gmail.com>
2016-11-02 16:53:40 +0700

    SSDC_UMP-40 - finish API : reboot, factory reset for deviceApiClient and HomeControlle class

--- src/main/java/vn/vnpt/ssdc/api/client/DeviceApiClient.java
+++ src/main/java/vn/vnpt/ssdc/api/client/DeviceApiClient.java
@@ -7,6 +7,10 @@ import org.springframework.web.client.RestTemplate;
 import vn.vnpt.ssdc.dto.AcsResponse;
 import vn.vnpt.ssdc.utils.StringUtils;
 
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -17,15 +21,62 @@ public class DeviceApiClient {
 
     private RestTemplate restTemplate;
     private String apiEndpointUrl;
+
     @Autowired
     public DeviceApiClient(@Value("${apiEndpointUrl}") String apiEndpointUrl) {
         this.restTemplate = new RestTemplate();
         this.apiEndpointUrl = apiEndpointUrl + "devices";
     }
 
-    public AcsResponse findDevices(Map<String,String> queryParams) {
+    public AcsResponse findDevices(Map<String, String> queryParams) {
         String queryString = StringUtils.queryStringFromMap(queryParams);
         String url = this.apiEndpointUrl + "?" + queryString;
-        return this.restTemplate.getForObject(url,AcsResponse.class,queryParams);
+        return this.restTemplate.getForObject(url, AcsResponse.class, queryParams);
+    }
+
+    /**
+     * Reboot device by device ID
+     *
+     * @param deviceId
+     * @param timeout
+     * @param isNow
+     * @return 202 if the tasks have been queued to be executed at the next inform.
+     * 404 Not found
+     * status code 200 if tasks have been successfully executed
+     */
+    public AcsResponse reboot(String deviceId, String timeout, String isNow) {
+        StringBuilder uri = new StringBuilder(apiEndpointUrl);
+        uri.append("/" + deviceId + "/reboot");
+
+        //Build query params
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("now", isNow);
+        queryParams.put("timeout", timeout);
+        String queryString = StringUtils.queryStringFromMap(queryParams);
+        uri.append("?" + queryString);
+        return this.restTemplate.postForObject(uri.toString(), null, AcsResponse.class, queryParams);
+    }
+
+    /**
+     * Factory device by device ID
+     *
+     * @param deviceId
+     * @param timeout
+     * @param isNow
+     * @return 202 if the tasks have been queued to be executed at the next inform.
+     * 404 Not found
+     * status code 200 if tasks have been successfully executed
+     */
+    public AcsResponse factoryReset(String deviceId, String timeout, String isNow) {
+        StringBuilder uri = new StringBuilder(apiEndpointUrl);
+        uri.append("/" + deviceId + "/factoryReset");
+
+        //Build query params
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("timeout", timeout + "");
+        queryParams.put("now", isNow + "");
+        String queryString = StringUtils.queryStringFromMap(queryParams);
+        uri.append("?" + queryString);
+        return this.restTemplate.postForObject(uri.toString(), null, AcsResponse.class, queryParams);
     }
 }
--- src/main/java/vn/vnpt/ssdc/controllers/HomeController.java
+++ src/main/java/vn/vnpt/ssdc/controllers/HomeController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import vn.vnpt.ssdc.api.client.DeviceApiClient;
 import vn.vnpt.ssdc.dto.AcsResponse;
 import vn.vnpt.ssdc.model.Device;
+import vn.vnpt.ssdc.utils.StringUtils;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,32 +27,71 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        Map<String,String> indexParams = indexParams();
+        Map<String, String> indexParams = indexParams();
 
         //query devices from acs
-        Map<String,String> query = new HashMap<String,String>();
+        Map<String, String> query = new HashMap<String, String>();
         query.put("parameters", String.join(",", indexParams.keySet()));
         //TODO add paging param
 
         AcsResponse response = deviceApiClient.findDevices(query);
 
-        List<Device> devices = Device.fromJsonString(response.body,indexParams.keySet());
+        List<Device> devices = Device.fromJsonString(response.body, indexParams.keySet());
 
         //add render args
-        model.addAttribute("total",response.nbOfItems);
-        model.addAttribute("indexParams",indexParams);
+        model.addAttribute("total", response.nbOfItems);
+        model.addAttribute("indexParams", indexParams);
         model.addAttribute("devices", devices);
         return "index";
     }
 
     //returns a map containing friendly name and actual path for parameters shown in device index view
     //TODO move this to configuration
-    private Map<String,String> indexParams() {
-        return new HashMap<String,String>() {{
-            put("_deviceId._SerialNumber","Serial Number");
-            put("_deviceId._Manufacturer","Manufacturer");
-            put("_deviceId._ProductClass","Product Class");
-            put("_lastInform","Last Inform");
+    private Map<String, String> indexParams() {
+        return new HashMap<String, String>() {{
+            put("_deviceId._SerialNumber", "Serial Number");
+            put("_deviceId._Manufacturer", "Manufacturer");
+            put("_deviceId._ProductClass", "Product Class");
+            put("_lastInform", "Last Inform");
         }};
     }
+
+    /**
+     * Reboot device by device ID
+     *
+     * @param deviceId
+     * @param timeout
+     * @param now      :now = true then reboot right now
+     * @return 202 if the tasks have been queued to be executed at the next inform.
+     * 404 Not found
+     * status code 200 if tasks have been successfully executed
+     */
+    @GetMapping("/devices/{deviceId}/reboot")
+    @ResponseBody
+    public AcsResponse reboot(@PathVariable String deviceId,
+                              @RequestParam(value = "timeout", defaultValue = "3000") String timeout,
+                              @RequestParam(value = "now", defaultValue = "false") String now) {
+        AcsResponse result = deviceApiClient.reboot(deviceId, timeout, now);
+        return result;
+    }
+
+
+    /**
+     * Factory device by device ID
+     *
+     * @param deviceId
+     * @param timeout
+     * @param now      :now = true then factoryReset right now
+     * @return 202 if the tasks have been queued to be executed at the next inform.
+     * 404 Not found
+     * status code 200 if tasks have been successfully executed
+     */
+    @GetMapping("/devices/{deviceId}/factoryReset")
+    @ResponseBody
+    public AcsResponse factoryReset(@PathVariable String deviceId,
+                                    @RequestParam(value = "timeout", defaultValue = "3000") String timeout,
+                                    @RequestParam(value = "now", defaultValue = "false") String now) {
+        return deviceApiClient.factoryReset(deviceId, timeout, now);
+    }
+
 }
